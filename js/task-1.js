1c1bb16f4ace0761c13293a694b4a68abfe50358
Mariia Shev <mariia.shevchenkoooo@gmail.com>
2026-08-21 18:50:31 +0300

    Finale?

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,21 @@
+// const listItems = document.querySelectorAll('#categories .item');
+// console.log(`Number of categories: ${listItems.length}`);
+
+// listItems.forEach(item => {
+//   const title = item.querySelector('h2');
+//   const categoryItems = item.querySelectorAll('.item ul li');
+
+//   console.log(`Categorie: ${title.textContent}`);
+//   console.log(`Elements: ${categoryItems.length}`);
+// });
+
 const listItems = document.querySelectorAll('#categories .item');
 console.log(`Number of categories: ${listItems.length}`);
 
 listItems.forEach(item => {
-  const title = item.querySelector('h2');
-  const categoryItems = item.querySelectorAll('.item ul li');
+  const title = item.querySelector('.categorie-title');
+  const categoryItems = item.querySelectorAll('.categorie-item');
 
-  console.log(`Categorie: ${title.textContent}`);
+  console.log(`Category: ${title.textContent}`);
   console.log(`Elements: ${categoryItems.length}`);
 });
