6c02fadd7a2552079651241aba8c76294ff6d824
Mariia Shev <mariia.shevchenkoooo@gmail.com>
2026-08-21 18:53:42 +0300

    .

--- js/task-1.js
+++ js/task-1.js
@@ -13,8 +13,8 @@ const listItems = document.querySelectorAll('#categories .item');
 console.log(`Number of categories: ${listItems.length}`);
 
 listItems.forEach(item => {
-  const title = item.querySelector('.categorie-title');
-  const categoryItems = item.querySelectorAll('.categorie-item');
+  const title = item.querySelector('h2');
+  const categoryItems = item.querySelectorAll('ul li');
 
   console.log(`Category: ${title.textContent}`);
   console.log(`Elements: ${categoryItems.length}`);
